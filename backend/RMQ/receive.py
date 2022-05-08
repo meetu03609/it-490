0b40e8a026b275cad4bb570e255cc86d8831a23b
Avnesh <asj8@njit.edu>
2022-05-08 17:15:30 -0400

    host changed for rmq

--- backend/RMQ/receive.py
+++ backend/RMQ/receive.py
@@ -4,7 +4,7 @@ import time
 
 credentials = pika.PlainCredentials('admin', 'admin')
 connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='172.17.0.1',
+    pika.ConnectionParameters(host='10.144.198.172',
     virtual_host='/',
     port=5672,
     credentials=credentials))
